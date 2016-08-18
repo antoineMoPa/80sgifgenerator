b6507adbb9eb7a912cdaf165a41e901fc804b73d
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2016-08-17 21:15:10 -0400

    nice shader editor here

--- js/glsl-math.js
+++ js/glsl-math.js
@@ -12,12 +12,14 @@
 
 
 var canvas = qsa(".result-canvas")[0];
-canvas.height = 512;
-canvas.width = 512;
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
 
 var ctx = canvas.getContext("webgl");
 
-var error_pre = qsa(".error-pre")[0];
+var fragment_error_pre = qsa(".fragment-error-pre")[0];
+var vertex_error_pre = qsa(".vertex-error-pre")[0];
 
 ctx.clearColor(0.0, 0.0, 0.0, 1.0);
 ctx.enable(ctx.DEPTH_TEST);
@@ -39,32 +41,57 @@ ctx.bufferData(ctx.ARRAY_BUFFER, new Float32Array(vertices), ctx.STATIC_DRAW);
 var vertex_code = qsa("textarea[name='vertex']")[0];
 var fragment_code = qsa("textarea[name='fragment']")[0];
 
-vertex_code.addEventListener("keyup",init_program);
-vertex_code.addEventListener("change",init_program);
-fragment_code.addEventListener("keyup",init_program);
-fragment_code.addEventListener("change",init_program);
+// Enable codemirror
+
+var v_editor = CodeMirror.fromTextArea(vertex_code, {
+    lineNumbers: true
+});
+
+var f_editor = CodeMirror.fromTextArea(fragment_code, {
+    lineNumbers: true
+});
+
+v_editor.on("change", init_program);
+f_editor.on("change", init_program);
 
 init_program();
 
+var program;
+
 function init_program(){
-    
-    var program = ctx.createProgram();
+    program = ctx.createProgram();
         
     var vertex_shader =
-        add_shader(ctx.VERTEX_SHADER, vertex_code.value);
+        add_shader(ctx.VERTEX_SHADER, v_editor.getValue());
     
     var fragment_shader =
-        add_shader(ctx.FRAGMENT_SHADER,fragment_code.value);
+        add_shader(ctx.FRAGMENT_SHADER, f_editor.getValue());
     
     function add_shader(type,content){
         var shader = ctx.createShader(type);
         ctx.shaderSource(shader,content);
         ctx.compileShader(shader);
+
+        // Find out right error pre
+        var type_pre = type == ctx.VERTEX_SHADER ?
+            vertex_error_pre:
+            fragment_error_pre;
+        
         if(!ctx.getShaderParameter(shader, ctx.COMPILE_STATUS)){
             var err = ctx.getShaderInfoLog(shader);
-            console.log(err);
-            error_pre.textContent = err;
+
+            // Find shader type
+            var type_str = type == ctx.VERTEX_SHADER ?
+                "vertex":
+                "fragment";
+            
+            type_pre.textContent =
+                "Error in " + type_str + " shader.\n" +
+                err;
+        } else {
+            type_pre.textContent = "";
         }
+        
         ctx.attachShader(program, shader);
         return shader;
     }
@@ -78,96 +105,22 @@ function init_program(){
     ctx.useProgram(program);
 
     var positionAttribute = ctx.getAttribLocation(program, "position");
+    
     ctx.enableVertexAttribArray(positionAttribute);
     ctx.vertexAttribPointer(positionAttribute, 3, ctx.FLOAT, false, 0, 0);
-    
+
     draw();
 }
     
 function draw(){
-    initDataTexture(ctx);
+    var timeAttribute = ctx.getUniformLocation(program, "time");
+    ctx.uniform1f(timeAttribute, parseFloat((new Date()).getTime() % 10000));
+    
     ctx.drawArrays(ctx.TRIANGLE_STRIP, 0, 4);
 
     ctx.viewport(0, 0, canvas.width, canvas.height);
+
+    window.requestAnimationFrame(draw);
 }
 
-
-/*
-  Creates the texture that contains the data
-  to pass to glsl. Assigns texture to uniform 'input-tex'
-  
- */
-function initDataTexture(ctx){
-    var gl = ctx;
-
-    var canvasTexture = qsa('#input_data')[0];
-    
-    var size = 512;
-    
-    canvasTexture.width = size;
-    canvasTexture.height = size;
-    
-    var texCtx = canvasTexture.getContext("2d");
-    
-    var imgdata = texCtx.createImageData(size, size);
-    var len = size * size * 4;
-    var data = imgdata.data;
-
-    // Create a circle for testing
-    for(var i = 0; i < size; i++){
-        for(var j = 0; j < size; j++){
-            var r = Math.sqrt(Math.pow(i-size/2,2) + Math.pow(j-size/2,2));
-            if(r < size/3){
-                set_one_pixel(i, j,
-                              255, 256 * (i < 256),255 * (j > 256), 255);
-            } else {
-                set_one_pixel(i, j,
-                              255 * (i>256), 255 * (i < 256),255 * (j > 256), 40);
-            }
-        }
-    }
-    
-    function set_one_pixel(i,j,valr,valg,valb,vala){
-        var index = 4 * (j * size + i);
-        // Set value
-        imgdata.data[index + 0] = valr;
-        imgdata.data[index + 1] = valg;
-        imgdata.data[index + 2] = valb;
-        imgdata.data[index + 3] = vala;
-    }
-
-    texCtx.putImageData(imgdata, 0, 0);
-    
-    initTexture();
-    
-    function initTexture() {
-        var tex = gl.createTexture();
-        handleLoadedTexture(tex, canvasTexture);
-    }
-    
-    // From: http://www.delphic.me.uk/webgltext.html
-    function handleLoadedTexture(texture, textureCanvas) {
-        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-        
-        gl.bindTexture(gl.TEXTURE_2D, texture);
-        
-        gl.texImage2D( gl.TEXTURE_2D,
-                       0,
-                       gl.RGBA,
-                       gl.RGBA,
-                       gl.UNSIGNED_BYTE,
-                       textureCanvas );
-        
-        gl.texParameteri( gl.TEXTURE_2D,
-                          gl.TEXTURE_MAG_FILTER,
-                          gl.LINEAR );
-        
-        gl.texParameteri( gl.TEXTURE_2D,
-                          gl.TEXTURE_MIN_FILTER,
-                          gl.LINEAR_MIPMAP_NEAREST );
-        
-        gl.generateMipmap(gl.TEXTURE_2D);
-        
-        gl.activeTexture(gl.TEXTURE0);
-    }
-}
+window.requestAnimationFrame(draw);
